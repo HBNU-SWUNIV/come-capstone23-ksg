dd8a823ed0b583b2ad05e69a53d3587198f947f3
kjh0117 <127067579+kjh0117@users.noreply.github.com>
2023-04-09 22:42:33 +0900

    Add files via upload

---
+++ 003 Code/frontend/src/components/PageFooter.vue
@@ -0,0 +1,15 @@
+<!-- PageFooter.vue -->
+<template>
+    <hr/>
+    <footer>
+      this is a footer.
+    </footer>
+  </template>
+  
+  <script>
+  export default {}
+  </script>
+  
+  <style scoped>
+  
+  </style>
