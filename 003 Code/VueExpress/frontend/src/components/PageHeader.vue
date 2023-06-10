551664bdd317796064dc16dc95ebe1c1970739e8
kjh0117 <127067579+kjh0117@users.noreply.github.com>
2023-06-11 02:02:32 +0900

    Update PageHeader.vue

--- 003 Code/VueExpress/frontend/src/components/PageHeader.vue
+++ 003 Code/VueExpress/frontend/src/components/PageHeader.vue
@@ -3,6 +3,7 @@
       <router-link class="menu" to="/">Home</router-link>
       <router-link to="/scedule">나만의 시간표</router-link>
       <router-link to="/board/list">게시판</router-link>
+      <router-link to="/book/list">서적평가</router-link>
       <router-link to="/login">로그인</router-link>
     </div>
 </template>
