9cbd8f66c4f23fafdc6fb50f4ce9340eda157ed4
20171584 <121631426+swacho@users.noreply.github.com>
2023-06-11 22:41:10 +0900

    Add files via upload

--- 003 Code/VueExpress/backend/app.js
+++ 003 Code/VueExpress/backend/app.js
@@ -11,6 +11,7 @@ var registerRouter = require('./routes/register');
 var boardlistRouter = require('./routes/boardlist');
 var commentlistRouter = require('./routes/commentlist');
 var schedulelistRouter = require('./routes/schedulelist');
+var booklistRouter = require('./routes/booklist');
 
 var app = express();
 
@@ -31,7 +32,7 @@ app.use('/api/login', loginRouter);
 app.use('/api/register', registerRouter);
 app.use('/api/boardlist', boardlistRouter);
 app.use('/api/commentlist', commentlistRouter);
-app.use('/api/schedulelist', schedulelistRouter);
+app.use('/api/booklist', booklistRouter);
 
 
 
