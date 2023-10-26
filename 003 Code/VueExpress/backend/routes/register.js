c943222b052c1c82af64c46d3f60aa21dfea472d
kjh0117 <127067579+kjh0117@users.noreply.github.com>
2023-10-26 23:16:21 +0900

    Delete 003 Code/VueExpress directory

--- 003 Code/VueExpress/backend/routes/register.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const control = require('../src/customer/control');
-router.post("/", control.registerCustomer); // 요청을 control.js 에서 처리함
-module.exports = router;
