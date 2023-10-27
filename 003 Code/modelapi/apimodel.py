b5a029ce61f87b5cfc7cfaf3f29f581251f38c80
wildtureky <127077710+wildtureky@users.noreply.github.com>
2023-10-27 22:08:16 +0900

    Add files via upload

--- 003 Code/modelapi/apimodel.py
+++ 003 Code/modelapi/apimodel.py
@@ -6,8 +6,10 @@ from torchvision import datasets,models, transforms
 import torch.nn as nn
 import os 
 import io
-from flask_cors import CORS
-
+from pandas import DataFrame
+from konlpy.tag import Mecab
+from tqdm import tqdm
+mecab = Mecab('C:/mecab/share/mecab-ko-dic')
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 app = Flask(__name__)
 currentPath=os.getcwd()
@@ -16,7 +18,7 @@ model = models.resnet34()
 
 num_features =model.fc.in_features
 
-model.fc = nn.Linear(num_features, 4)
+model.fc = nn.Linear(num_features, 7)
 model = model.to(device)
 model.load_state_dict(torch.load(currentPath+'/model.pt'), strict=False)
 
@@ -28,7 +30,7 @@ transforms_test = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-class_id = ['Spider', 'mouse', 'nomal', 'snake']
+class_id = ['Spider', 'centipede', 'mouse', 'nomal', 'scorpion', 'snake', 'worm']
 def get_classfication(image_bytes):
     img = Image.open(io.BytesIO(image_bytes))
     img = transforms_test(img).unsqueeze(0).to(device)
@@ -38,13 +40,16 @@ def get_classfication(image_bytes):
 
     return class_id[preds[0]]
 
-@app.route("/averCF", methods=['POST'])
+
+@app.route("/classfication", methods=['POST'])
 def averclassfication():
     file= request.files['image']
     image_bytes = file.read()
     class_id =get_classfication(image_bytes=image_bytes)
     print(class_id)
     return jsonify({'class_id':class_id})
+
+
 
 if __name__ == '__main__':
     app.run()
